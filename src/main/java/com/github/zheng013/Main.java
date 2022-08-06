c1482cb52170cfbc37f3685cfb13aced3a166e6a
zhenglijian <zhenglijian@gyenno.com>
2022-08-06 18:12:52 +0800

    perf():进行基本的重构，超长方法的抽离，使用流式streamApi对循环遍历进行改写[]

--- src/main/java/com/github/zheng013/Main.java
+++ src/main/java/com/github/zheng013/Main.java
@@ -36,34 +36,51 @@ public class Main {
                 continue;
             }
             //判断是否为我们想要处理的链接 暂时只处理 news.sina.cn 并且过滤掉一些不为新闻的链接
-            if ((link.contains("news.sina.cn") && !link.contains("passport.sina.cn")) || "https://sina.cn" == link) {
-
+            if (isInterestingLink(link)) {
                 //只有我们感兴趣的才处理他
                 // 获取可用的a标签的href放入到链接池linkPool中
-                CloseableHttpClient httpClient = HttpClients.createDefault();
-                HttpGet httpGet = new HttpGet(link);
-                // user-agent 代理添加表明为浏览器访问
-                httpGet.setHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
-                System.out.println(link);
-                try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-                    System.out.println(response.getStatusLine());
-                    HttpEntity responseEntity = response.getEntity();
-                    String htmlString = EntityUtils.toString(responseEntity);
-                    Document doc = Jsoup.parse(htmlString);
-                    ArrayList<Element> aTags = doc.select("a");
-                    for (Element aTag : aTags) {
-                        linkPool.add(aTag.attr("href"));
-                    }
-                    //如果是一个新闻详情的界面 就存入数据库 否则什么都不做
-                    ArrayList<Element> articleTags = doc.select("article");
-                    for (Element articleTag : articleTags) {
-                      //通过分析新闻的详情界面，分析接口并获取相关标题文本数据
-                        String title = articleTag.child(0).text();
-                        System.out.println(title);
-                    }
-                    processedLinks.add(link);
+                Document doc = httpGetAndHtmlParse(link);
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                //如果是一个新闻详情的界面 就存入数据库 否则什么都不做
+                ArrayList<Element> articleTags = doc.select("article");
+                for (Element articleTag : articleTags) {
+                    //通过分析新闻的详情界面，分析接口并获取相关标题文本数据
+                    String title = articleTag.child(0).text();
+                    System.out.println(title);
                 }
+                processedLinks.add(link);
             }
         }
+    }
+
+    private static Document httpGetAndHtmlParse(String link) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        // user-agent 代理添加表明为浏览器访问
+        httpGet.setHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+        System.out.println(link);
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            System.out.println(response.getStatusLine());
+            HttpEntity responseEntity = response.getEntity();
+            String htmlString = EntityUtils.toString(responseEntity);
+            return Jsoup.parse(htmlString);
+
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) && isNotLoginPage(link)) || isIndexPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn" == link;
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
     }
 }
