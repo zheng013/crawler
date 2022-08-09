d080083ecd9f3e3b7a1f4e4bb1094d67716c4048
zhenglijian <zhenglijian@gyenno.com>
2022-08-09 18:08:14 +0800

    perf():引入H2数据库，从数据库中获取相关数据。 对于数据操作的sql需要单独封装成方法调用。

--- src/main/java/com/github/zheng013/Main.java
+++ src/main/java/com/github/zheng013/Main.java
@@ -11,28 +11,53 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static List<String> loadsDataFromDatabaseBySql(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<String>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        }
+        return results;
+    }
 
-        List<String> linkPool = new ArrayList<String>();
-        Set<String> processedLinks = new HashSet<String>();
-        linkPool.add("https://sina.cn");
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:C:\\Users\\gyenno\\Desktop\\crawler\\news", "root", "root");
+        //从数据库中加载即将处理的链接
+        Set<String> processedLinks = new HashSet<String>(loadsDataFromDatabaseBySql(connection, "select link from links_to_be_processed"));
         while (true) {
+            List<String> linkPool = loadsDataFromDatabaseBySql(connection, "select link from links_to_be_processed");
             if (linkPool.isEmpty()) {
                 break;
             }
             //获取link链接之后需要立刻进行删除。从ArrayList尾部进行删除更有效率 remove删除相关索引值并返回对应的元素
+            //处理完链接之后出待处理的数据库池中删除
             String link = linkPool.remove(linkPool.size() - 1);
+            try (PreparedStatement statement = connection.prepareStatement("delete from links_to_be_processed where link = ?")) {
+                statement.setString(1, link);
+                statement.executeUpdate();
+            }
             if (link.startsWith("//")) {
                 link = "https:" + link;
             }
-            //判断链接是否被处理过
-            if (processedLinks.contains(link)) {
+            //判断链接是否被处理过  通过读取数据判断当前链接是否被处理过
+            Boolean flag = false;
+            try (PreparedStatement statement = connection.prepareStatement("select link from links_already_processed where link=?")) {
+                statement.setString(1, link);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    flag = true;
+                }
+            }
+            if (flag) {
                 continue;
             }
             //判断是否为我们想要处理的链接 暂时只处理 news.sina.cn 并且过滤掉一些不为新闻的链接
@@ -40,7 +65,13 @@ public class Main {
                 //只有我们感兴趣的才处理他
                 // 获取可用的a标签的href放入到链接池linkPool中
                 Document doc = httpGetAndHtmlParse(link);
-                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                for (Element aTag : doc.select("a")) {
+                    String href = aTag.attr("href");
+                    try (PreparedStatement statement = connection.prepareStatement("insert into links_to_be_processed(link) values(?)")) {
+                        statement.setString(1, href);
+                        statement.executeUpdate();
+                    }
+                }
                 //如果是一个新闻详情的界面 就存入数据库 否则什么都不做
                 ArrayList<Element> articleTags = doc.select("article");
                 for (Element articleTag : articleTags) {
@@ -48,10 +79,15 @@ public class Main {
                     String title = articleTag.child(0).text();
                     System.out.println(title);
                 }
-                processedLinks.add(link);
+                System.out.println(link);
+                try (PreparedStatement statement = connection.prepareStatement("insert into links_already_processed(link) values(?)")) {
+                    statement.setString(1, link);
+                    statement.executeUpdate();
+                }
             }
         }
     }
+
 
     private static Document httpGetAndHtmlParse(String link) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
