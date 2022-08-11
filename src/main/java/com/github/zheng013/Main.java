e31a043f19089a333e26b75beb49ed3ff99a60d8
zhenglijian <zhenglijian@gyenno.com>
2022-08-11 14:31:07 +0800

    perf():使用DatabaseAccessObject实例化的dao对象对数据库进行操作，将jdbc的连接初始化放到其内部的构造函数中执行[]

--- src/main/java/com/github/zheng013/Main.java
+++ src/main/java/com/github/zheng013/Main.java
@@ -17,27 +17,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static String getNextLink(Connection connection, String sql) throws SQLException {
-        String link = null;
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                link = resultSet.getString(1);
-            }
-        }
-        return link;
-    }
+
+    private static final DatabaseAccessObject dao = new DatabaseAccessObject();
 
     public static void main(String[] args) throws IOException, SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:h2:file:C:\\Users\\gyenno\\Desktop\\crawler\\news", "root", "root");
         //从数据库中加载即将处理的链接
         String link = null;
-        while ((link = getNextLinkAndDeleteFromDatabase(connection)) != null) {
+        while ((link = dao.getNextLinkAndDeleteFromDatabase()) != null) {
             if (link.startsWith("//")) {
                 link = "https:" + link;
             }
             //判断链接是否被处理过  通过读取数据判断当前链接是否被处理过
-            if (isLinkProcessed(connection, link)) {
+            if (dao.isLinkProcessed(link)) {
                 continue;
             }
             //判断是否为我们想要处理的链接 暂时只处理 news.sina.cn 并且过滤掉一些不为新闻的链接
@@ -45,67 +36,15 @@ public class Main {
                 //只有我们感兴趣的才处理他
                 // 获取可用的a标签的href放入到链接池linkPool中
                 Document doc = httpGetAndHtmlParse(link);
-                parseUrlsFromPageStoreIntoDatabase(connection, doc);
+                dao.parseUrlsFromPageStoreIntoDatabase(doc);
                 //如果是一个新闻详情的界面 就存入数据库 否则什么都不做
-                storeLinkIntoDatabaseIfIsNewsPage(doc, connection, link);
+                dao.storeLinkIntoDatabaseIfIsNewsPage(doc, link);
                 System.out.println(link);
-                updateDatabase(connection, link, "insert into links_already_processed(link) values(?)");
+                dao.updateDatabase(link, "insert into links_already_processed(link) values(?)");
             }
         }
     }
 
-    private static String getNextLinkAndDeleteFromDatabase(Connection connection) throws SQLException {
-        String link = getNextLink(connection, "select link from links_to_be_processed limit 1");
-        if (null != link) {
-            updateDatabase(connection, link, "delete from links_to_be_processed where link = ?");
-        }
-        //获取link链接之后需要立刻进行删除。从ArrayList尾部进行删除更有效率 remove删除相关索引值并返回对应的元素
-        //处理完链接之后出待处理的数据库池中删除
-        return link;
-    }
-
-    private static void parseUrlsFromPageStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
-        for (Element aTag : doc.select("a")) {
-            String href = aTag.attr("href");
-            updateDatabase(connection, href, "insert into links_to_be_processed(link) values(?)");
-        }
-    }
-
-    private static void updateDatabase(Connection connection, String link, String sql) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, link);
-            statement.executeUpdate();
-        }
-    }
-
-    private static Boolean isLinkProcessed(Connection connection, String link) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("select link from links_already_processed where link=?")) {
-            statement.setString(1, link);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static void storeLinkIntoDatabaseIfIsNewsPage(Document doc, Connection connection, String link) throws SQLException {
-        ArrayList<Element> articleTags = doc.select("article");
-        for (Element articleTag : articleTags) {
-            //通过分析新闻的详情界面，分析接口并获取相关标题文本数据
-            String title = articleTag.child(0).text();
-            List<Element> paragraphs = articleTag.select("p");
-            String content = paragraphs.stream().map(Element::text).collect(Collectors.joining("\n"));
-            try (PreparedStatement statement = connection.prepareStatement("insert into news(title,content,url,created_at,modified_at) values(?,?,?,now(),now())")) {
-                statement.setString(1, title);
-                statement.setString(2, content);
-                statement.setString(3, link);
-                statement.executeUpdate();
-            }
-            System.out.println(content);
-            System.out.println(title);
-        }
-    }
 
     private static Document httpGetAndHtmlParse(String link) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
