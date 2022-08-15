77f8b7924efe50084a014608b17d9481edc2fda5
zhenglijian <zhenglijian@gyenno.com>
2022-08-15 13:39:10 +0800

    perf():添加created_at modified_at的default默认值，避免执行flyway迁移相关的默认值初始化报错[]

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,3 +1,3 @@
 CREATE TABLE LINKS_TO_BE_PROCESSED(link varchar(2000));
 CREATE TABLE LINKS_ALREADY_PROCESSED(link varchar(2000));
-CREATE TABLE NEWS(id bigint PRIMARY KEY auto_increment,title text,content text,url varchar(2000),created_at timestamp,modified_at timestamp );
+CREATE TABLE NEWS(id bigint PRIMARY KEY auto_increment,title text,content text,url varchar(2000),created_at timestamp default now(),modified_at timestamp default now());
